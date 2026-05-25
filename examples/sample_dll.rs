2715824044bd3c347bb704768bc409034a91f6bf
tsmckinney <tsmckinney10@gmail.com>
2026-05-25 18:05:19 -0400

    Marked no_mangle as unsafe.

--- examples/sample_dll.rs
+++ examples/sample_dll.rs
@@ -7,7 +7,7 @@ fn hack_thread() {
     println!("Hi from the sample dll");
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "system" fn DllMain(_module_handle: HModule, dw_reason: DWORD, _lp_reserved: LPVOID) -> bool {
     match dw_reason {
         1u32 => {
