7da1922499d958c08eae3368fea3ab32f81e7cb0
tsmckinney <tsmckinney10@gmail.com>
2026-05-26 17:53:35 -0400

    Set self.method_table to the method table received from the graphics API hook.

--- marauder/src/hooks/mod.rs
+++ marauder/src/hooks/mod.rs
@@ -90,6 +90,7 @@ impl GraphicsHook {
                 hooks::d3d::get_method_table(render_type)?
             },
         };
+        self.method_table = method_table;
 
         Ok(Self {
             method_table,
